036bdcd880f60a9eb78d225d000af13584a517f4
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-03-20 19:19:09 +0300

    HW Task10 done

--- Homeworck02/Task14.py
+++ Homeworck02/Task14.py
@@ -1,2 +1,16 @@
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k),
 #  не превосходящие числа N.
+
+
+N = int(input("Введите число N: "))
+sum = 2
+number = 1
+if sum <= N:
+    while sum < N:
+        sum = sum ** number
+        if sum < N:
+            print(sum)
+            number = number + 1
+
+else :
+    print("не верное значение")
