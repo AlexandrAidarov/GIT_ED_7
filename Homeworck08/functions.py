dc309bce90de54f457d9cc73eb5df6ef4c8790f4
Alexandr Aidarov <aidarovi7ia@mail.ru>
2023-04-17 18:14:26 +0300

    ED7 done

--- Homeworck08/functions.py
+++ Homeworck08/functions.py
@@ -10,19 +10,46 @@ def rewrite_data():
     tel_boock = search2(tel_boock, data1, data2)
     
     with open('boock.txt', 'w', encoding='utf-8') as f:
-        f.write('\n'.join([post for post in tel_boock]))
+        f.write('\n'([post for post in tel_boock]))
     print('Успешно')
     
 def search2(boock: str, info1, info2) -> str:
     boock = boock.split()
-    boock1 = ""
+    count = 0
+    boock1 = [len(boock) / 3]
+    n = 0
+    Ni = ''
     for i in boock:
+        
+        count += 1
         if info1 in i:
             i = info2
-        # boock1 += i    
+        Ni += i
+        if count % 3 == 0:
+            boock1[n] == Ni
+            if n < len(boock1)-1:
+             n +=1  
+    return boock1
+
+def del_data():
+    "осущ. удаление в справочнике"
+    data1 = input('введите данные которые нужно удалить :')   
+    
+    with open('boock.txt', 'r', encoding='utf-8') as f:
+        tel_boock = f.read()
+    tel_boock = search_del(tel_boock, data1)
+    
+    with open('boock.txt', 'w', encoding='utf-8') as f:
+        f.write('\n'.join([post for post in tel_boock]))
+    print('Успешно')
+    
+def search_del(boock: str, info1) -> str:
+    boock = boock.split()
+    info_del = ""
+    for i in boock:
+        if info1 in i:
+            i = info_del   
     return boock
-
-
 
 
 def show_data():
